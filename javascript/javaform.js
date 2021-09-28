ae0bd917de92ad70debc796577afbb8abe2240e5
Rurucode <rubenlage12@gmail.com>
2021-09-28 17:24:09 +0200

    Empiezo el Quizz

---
+++ javascript/javaform.js
@@ -0,0 +1,12 @@
+let respuestas = document.getElementsByClassName("cajarespuesta")
+let valorCorrecto = document.getElementById("").value
+
+let bienMalColorear = (texto) => {
+    if(texto == correcta) {
+
+    }
+}
+for(let i = 0; i < respuestas.length; i++) {
+    respuestas.addEventListener("click", bienMalColorear())
+}
+
