9414d54b9c58814178a58bab0475ed92aa8e3942
Rurucode <rubenlage12@gmail.com>
2021-09-29 20:59:49 +0200

    Arreglado pero revisar

--- javascript/javaform.js
+++ javascript/javaform.js
@@ -2,20 +2,20 @@ let losInputs = document.getElementsByClassName("inPut")
 let respuestaCorrecta = "Guerrero"
 let inputId = document.getElementById
 
-let funcionCambioColor = () => {
+let funcionCambioColor = (i) => {
   for(let i = 0; i < losInputs.length; i++) {
     if(losInputs[i].value == respuestaCorrecta) {
-      losInputs[i].style.backgroundColor = "green";
+       losInputs[i].style.backgroundColor = "green";
     }else{
-      losInputs[i].style.backgroundColor = "red";
+       losInputs[i].style.backgroundColor = "red";
     }
   }
 }
 
-let eventoStart = () =>{
-    for(let i = 0; i < losInputs.length; i++) {
-        losInputs[i].addEventListener("click", funcionCambioColor)
-    }
-}
 
-eventoStart();
+for(let i = 0; i < losInputs.length; i++) {
+   losInputs[i].addEventListener("click", funcionCambioColor)
+    }
+
+
+    
